03496bdca5fda5879780d9331cf60ef99ddef926
LeonDu <du_8yu@163.com>
2016-08-01 13:33:24 +0800

    加入一部分美工

---
+++ app/src/main/java/com/ecnu/leondu/sheetmusic/ReadingActivity.java
@@ -0,0 +1,58 @@
+package com.ecnu.leondu.sheetmusic;
+
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class ReadingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reading);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        // 提供沉浸式状态栏
+        {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                Window window = getWindow();
+                window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
+                        | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+                window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+                window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+                //获取样式中的属性值
+                TypedValue typedValue = new TypedValue();
+                this.getTheme().resolveAttribute(android.R.attr.colorAccent, typedValue, true);
+                int[] attribute = new int[]{android.R.attr.colorAccent};
+                TypedArray array = this.obtainStyledAttributes(typedValue.resourceId, attribute);
+                int color = array.getColor(0, Color.TRANSPARENT);
+                array.recycle();
+                window.setStatusBarColor(color);
+            }
+        }
+
+        setSupportActionBar(toolbar);
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
